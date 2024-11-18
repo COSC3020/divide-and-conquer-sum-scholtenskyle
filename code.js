35ad116087814d542d5ff17a9618f617760177ab
scholtenskyle <35465569+scholtenskyle@users.noreply.github.com>
2024-11-17 18:49:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length == 0){
+        return 0;
+    }
+    if (a.length == 1){
+        return a[0];
+    } 
+    if (a.length == 2){
+        return a[0] + a[1];
+    }
+
+    const thirdFrac = Math.floor(a.length / 3);
+    const first = divideAndConquerSum(a.slice(0, thirdFrac));
+    const center = divideAndConquerSum(a.slice(split, (thirdFrac * 2)));
+    const last = divideAndConquerSum(a.slice(thirdFrac * 2));
+    
+    return first + center + last;
 }
